4d1b5db79d210aff0f02f580cb852cc707c9e6c2
LINGAP <lma1169949842@gmail.com>
2020-04-09 21:56:53 -0500

    added neworder activity

---
+++ app/src/main/java/com/fullstack/frontend/NewOrderActivity.java
@@ -0,0 +1,14 @@
+package com.fullstack.frontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_order);
+    }
+}
