789e9c7491a5c3548648fa5aaea43b02dd782c43
LINGAP <lma1169949842@gmail.com>
2020-04-09 23:49:05 -0500

    added recommend placeholder

---
+++ app/src/main/java/com/fullstack/frontend/ui/newOrder/NewOrderInfoViewModel.java
@@ -0,0 +1,7 @@
+package com.fullstack.frontend.ui.newOrder;
+
+import androidx.lifecycle.ViewModel;
+
+public class NewOrderInfoViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/fullstack/frontend/ui/newOrder/NewOrderRecommend.java
@@ -0,0 +1,51 @@
+package com.fullstack.frontend.ui.newOrder;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+
+import com.fullstack.frontend.R;
+
+public class NewOrderRecommend extends DialogFragment {
+
+    private NewOrderInfoViewModel mViewModel;
+
+    public static NewOrderRecommend newInstance() {
+        return new NewOrderRecommend();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.new_order_recommend_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(NewOrderInfoViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // The only reason you might override this method when using onCreateView() is
+        // to modify any dialog characteristics. For example, the dialog includes a
+        // title by default, but your custom layout might not need it. So here you can
+        // remove the dialog title, but you must call the superclass to get the Dialog.
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        return dialog;
+    }
+
+}
