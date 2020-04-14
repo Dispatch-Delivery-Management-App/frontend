c37189d97535033bb2a16a0b0e577ee1f5ea4532
Novaaaaaa <zd700@nyu.edu>
2020-04-13 23:50:48 -0400

    update fragment_home

--- app/src/main/java/com/fullstack/frontend/ui/newOrder/NewOrderRecommendViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.fullstack.frontend.ui.newOrder;
-
-import androidx.lifecycle.ViewModel;
-
-public class NewOrderRecommendViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/com/fullstack/frontend/ui/tracking/OrderDetailViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.fullstack.frontend.ui.tracking;
-
-import androidx.lifecycle.ViewModel;
-
-public class OrderDetailViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
