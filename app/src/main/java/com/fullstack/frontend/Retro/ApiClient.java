44b00827d13c9f3c0f7ac34500fddbc160e90433
LINGAP <lma1169949842@gmail.com>
2020-04-16 19:30:05 -0500

    resolve conflict

--- app/src/main/java/com/fullstack/frontend/Retro/ApiClient.java
+++ app/src/main/java/com/fullstack/frontend/Retro/ApiClient.java
@@ -1,7 +1,5 @@
 package com.fullstack.frontend.Retro;
 
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
