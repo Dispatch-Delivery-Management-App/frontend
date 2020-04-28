f520867ad6f4045af8e580b9752f362be9fcdfbd
Novaaaaaa <zd700@nyu.edu>
2020-04-28 17:12:35 -0400

    add notification

---
+++ app/src/main/java/com/fullstack/frontend/DispatchApplication.java
@@ -0,0 +1,14 @@
+package com.fullstack.frontend;
+
+import android.app.Application;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class DispatchApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Stetho.initializeWithDefaults(this);
+        FirebaseMessaging.getInstance().subscribeToTopic("android");
+    }
+}
--- app/src/main/java/com/fullstack/frontend/MainActivity.java
+++ app/src/main/java/com/fullstack/frontend/MainActivity.java
@@ -1,5 +1,6 @@
 package com.fullstack.frontend;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
@@ -8,7 +9,10 @@ import android.view.Menu;
 
 import com.fullstack.frontend.Retro.ApiClient;
 import com.fullstack.frontend.Retro.ApiInterface;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
@@ -35,6 +39,52 @@ public class MainActivity extends AppCompatActivity {
 
         setupNavigation();
 
+//        Button logTokenButton = findViewById(R.id.logTokenButton);
+//        logTokenButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // Get token
+//                // [START retrieve_current_token]
+//                FirebaseInstanceId.getInstance().getInstanceId()
+//                        .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+//                            @Override
+//                            public void onComplete(@NonNull Task<InstanceIdResult> task) {
+//                                if (!task.isSuccessful()) {
+//                                    Log.w(TAG, "getInstanceId failed", task.getException());
+//                                    return;
+//                                }
+//
+//                                // Get new Instance ID token
+//                                String token = task.getResult().getToken();
+//
+//                                // Log and toast
+//                                String msg = getString(R.string.msg_token_fmt, token);
+//                                Log.d(TAG, msg);
+//                                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+//                            }
+//                        });
+//                // [END retrieve_current_token]
+//            }
+//        });
+
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "getInstanceId failed", task.getException());
+                            return;
+                        }
+
+                        // Get new Instance ID token
+                        String token = task.getResult().getToken();
+
+                        // Log and toast
+                        String msg = getString(R.string.msg_token_fmt, token);
+                        Log.d(TAG, msg);
+                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
 //    /**
@@ -103,5 +153,6 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+    
 
 }
