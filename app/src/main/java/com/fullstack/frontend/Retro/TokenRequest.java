b64243be9748e9f0660c430c00b8f43f3a99493b
Novaaaaaa <zd700@nyu.edu>
2020-04-29 19:35:44 -0400

    notification

--- app/src/main/java/com/fullstack/frontend/Retro/TokenRequest.java
+++ app/src/main/java/com/fullstack/frontend/Retro/TokenRequest.java
@@ -4,7 +4,8 @@ class TokenRequest {
     public int user_id;
     public String token;
 
-    public TokenRequest(token) {
+    public TokenRequest(int user_id, String token) {
+        this.user_id = user_id;
         this.token = token;
     }
 
